3c7426830af58ddf9ac173a5df256138dbb02b6b
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2024-06-25 18:25:45 +0530

    track update

--- src/components/track/Track.jsx
+++ src/components/track/Track.jsx
@@ -15,8 +15,8 @@ function Track() {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                 </svg>
 
-                                <h2 className="title-font font-medium text-lg text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>Premium Tshirts</h2>
-                                <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                                <h2 className="title-font font-medium text-lg text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>Nature's Shield</h2>
+                                <p className="leading-relaxed">Organic Pesticides for a Thriving Garden
                                 </p>
                             </div>
                         </div>
@@ -26,8 +26,8 @@ function Track() {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                 </svg>
 
-                                <h2 className="title-font font-medium text-lg text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>Premium Tshirts</h2>
-                                <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                                <h2 className="title-font font-medium text-lg text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>Revolutionary Organic Formulas</h2>
+                                <p className="leading-relaxed">say goodbye to harmful synthetic pesticides
                                 </p>
                             </div>
                         </div>
@@ -38,8 +38,8 @@ function Track() {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                 </svg>
 
-                                <h2 className="title-font font-medium text-lg text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>Premium Tshirts</h2>
-                                <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                                <h2 className="title-font font-medium text-lg text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>Sustainable Farming Starts Here</h2>
+                                <p className="leading-relaxed">Organic Pesticides for the Modern Grower
                                 </p>
                             </div>
                         </div>
