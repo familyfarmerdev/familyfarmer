68ba69f8490cb6594368694ea51394fdb3f69219
familyfarmerdev <159577740+familyfarmerdev@users.noreply.github.com>
2024-07-04 13:13:35 +0530

    Update Herosection.jsx

--- src/components/heroSection/Herosection.jsx
+++ src/components/heroSection/Herosection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <div>
-        <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" />
+        <img src="https://www.creatopy.com/blog/wp-content/uploads/2018/05/animations-e-commerce.png" alt="" />
     </div>
   )
 }
